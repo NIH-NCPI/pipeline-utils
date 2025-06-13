5e42361365517b94351f2f0f5ade7432004fe582
brendagutman <brenda.gutman@gmail.com>
2025-06-12 21:35:59 -0500

    Replace backslash

--- src/dbt_pipeline_utils/scripts/helpers/pipeline_docs_generation/generate_common_docs.py
+++ src/dbt_pipeline_utils/scripts/helpers/pipeline_docs_generation/generate_common_docs.py
@@ -305,11 +305,10 @@ class DocGeneration():
                 column_definitions.append(f'"{col_name}"::{sql_type} as "{column_name_code}"')
 
             sql_content = f"""{{{{ config(materialized='table') }}}}
-            backslash_char = "\"
 
     with source as (
         select 
-        {",{backslash_char}n       ".join(column_definitions)}
+        {",\n       ".join(column_definitions)}
         from {{{{ source('{self.study_id}','{src_table}') }}}}
     )
 
