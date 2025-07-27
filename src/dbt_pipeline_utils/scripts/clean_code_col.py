d2195bf7cbbf3fd5d996b20c705f2c17bc18c194
brendagutman <brenda.gutman@gmail.com>
2025-07-27 14:31:09 -0500

    Add curie cleaning

--- src/dbt_pipeline_utils/scripts/clean_code_col.py
+++ src/dbt_pipeline_utils/scripts/clean_code_col.py
@@ -9,10 +9,12 @@ from dbt_pipeline_utils.scripts.helpers.general import *
 from dbt_pipeline_utils.scripts.helpers.common import *
 from dbt_pipeline_utils import logger
 
-def clean_codes(codes):
-    codes = codes.replace('HP','|HP')
-    codes = codes.replace('HP','HP:')
-    codes = codes.replace('HP::','HP:')
+def clean_codes(codes, curies):
+    
+    for c in curies:
+        codes = codes.replace(c,f'|{c}')
+        codes = codes.replace(c,f'{c}:')
+        codes = codes.replace(f'{c}::',f'{c}:')
     codes = codes.replace(' ', '')  
     codes = codes.replace("''", '')  
     codes = codes.replace('"', '') 
@@ -31,14 +33,14 @@ def create_flag_column(codes):
     # Check if each code matches the valid format
     return [is_valid_format(code) for code in codes.split('|') if code.strip()]
     
-def main(df,column):
+def main(df,column,curies):
     df = read_file(df)
 
-    df['cleaned_col'] = df[column].apply(clean_codes)
+    df['cleaned_col'] = df[column].apply(lambda x: clean_codes(x, curies))
     df['correct_format'] = df['cleaned_col'].apply(create_flag_column)
 
-    # t=df[['cleaned_col','correct_format']]
-    # t.to_csv('col_clean.csv', index=False)
+    t=df[['cleaned_col','correct_format']]
+    t.to_csv('col_clean.csv', index=False)
 
     # Catch all codes that might not be cleaned properly
     questionable = df[df['correct_format'].apply(lambda x: not all(x))]
@@ -53,8 +55,13 @@ if __name__ == "__main__":
     
     parser.add_argument("-df", "--data_file", required=True, help="File containing the codes requiring metadata. Format: 'path/to/datafile.csv'")
     parser.add_argument("-c", "--column", required=True, help="Column name containing the codes requiring metadata. The utils can do some amount of cleaning. # Format: 'ExactFieldName'")
+    parser.add_argument("-o", "--ontologies", required=True, help="List of ontology prefixes")
+
     args = parser.parse_args()
 
+    curies=args.ontologies.split(',')
+
     main(df=args.data_file,
-         column=args.column
+         column=args.column,
+         curies=curies
          )
