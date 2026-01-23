189f6031d63f0442b9da0c084422c801d72d9ccb
brendagutman <brenda.gutman@gmail.com>
2026-01-23 12:41:34 -0600

    Edit int and exp yaml naming

--- src/dbt_pipeline_utils/p_utils/factory_functions.py
+++ src/dbt_pipeline_utils/p_utils/factory_functions.py
@@ -110,7 +110,7 @@ class PipelineObject:
         Load a secondary YAML config once paths are initialized.
         """
         config_path = (
-            static_dir / f"static/common_data_models/internal/metadata/{self.int_model_name}/_{self.int_model_name}_study.yaml"
+            static_dir / f"static/common_data_models/internal/metadata/_{self.int_model_name}/_{self.int_model_name}_study.yaml"
         )
         if not config_path.exists():
             raise FileNotFoundError(f"Config not found: {config_path}")
@@ -123,7 +123,7 @@ class PipelineObject:
         Load a secondary YAML config once paths are initialized.
         """
         config_path = (
-            static_dir / f"static/common_data_models/export/metadata/{self.exp_model_name}/_{self.exp_model_name}_study.yaml"
+            static_dir / f"static/common_data_models/export/metadata/_{self.exp_model_name}/_{self.exp_model_name}_study.yaml"
         )
 
         if not config_path.exists():
